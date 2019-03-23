30c6b5f3b581d327a83843d87c83c6162719bdd3
huangkun <huangkuna@gmail.com>
2019-03-24 02:31:13 +0800

    Updates

---
+++ precache-manifest.12e630fc873d60001b53ab8b787eb3a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "4880a428b016a8f449ef",
+    "url": "/static/js/5.48081043.chunk.js"
+  },
+  {
+    "revision": "17141275febdf09c9819",
+    "url": "/static/css/main.04a7120a.chunk.css"
+  },
+  {
+    "revision": "7fd61b851c72d0c72e83",
+    "url": "/static/js/runtime~main.cace4f89.js"
+  },
+  {
+    "revision": "dc38f3d38790547bc695",
+    "url": "/static/js/2.2ed025b9.chunk.js"
+  },
+  {
+    "revision": "f25b2c8c8168313d570820fe359998c5",
+    "url": "/static/media/cart-box-empty.f25b2c8c.png"
+  },
+  {
+    "revision": "2d3224941164b8431225",
+    "url": "/static/js/3.91f51c0b.chunk.js"
+  },
+  {
+    "revision": "00b79aea90705960523fed49f62621a7",
+    "url": "/static/media/ui.00b79aea.png"
+  },
+  {
+    "revision": "058f3594f49bfaae683e",
+    "url": "/static/js/4.a30411e3.chunk.js"
+  },
+  {
+    "revision": "655517bcca16db1f7aaa",
+    "url": "/static/js/8.99f4e57b.chunk.js"
+  },
+  {
+    "revision": "17141275febdf09c9819",
+    "url": "/static/js/main.f0e7e3e9.chunk.js"
+  },
+  {
+    "revision": "f824ae62d3f249268ab8",
+    "url": "/static/js/7.be24ea04.chunk.js"
+  },
+  {
+    "revision": "da62f65fbbff45e9d356",
+    "url": "/static/js/6.fb999422.chunk.js"
+  },
+  {
+    "revision": "655517bcca16db1f7aaa",
+    "url": "/static/css/8.d25e0afa.chunk.css"
+  },
+  {
+    "revision": "f824ae62d3f249268ab8",
+    "url": "/static/css/7.04b23d48.chunk.css"
+  },
+  {
+    "revision": "da62f65fbbff45e9d356",
+    "url": "/static/css/6.e1f6957d.chunk.css"
+  },
+  {
+    "revision": "4880a428b016a8f449ef",
+    "url": "/static/css/5.41bc7171.chunk.css"
+  },
+  {
+    "revision": "058f3594f49bfaae683e",
+    "url": "/static/css/4.65c7a85a.chunk.css"
+  },
+  {
+    "revision": "2d3224941164b8431225",
+    "url": "/static/css/3.3c9ee5dc.chunk.css"
+  },
+  {
+    "revision": "32bb8cd26f058886404bebb1038eaff4",
+    "url": "/index.html"
+  }
+];
